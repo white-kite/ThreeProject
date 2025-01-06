aca86c6fb146a9b6c58e855cbef8bc88580a2cdc
white-kite <bjw0103@naver.com>
2025-01-06 10:25:31 +0900

    첫 화면 open.html로 변경

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,8 @@ module.exports = {
 	// 단, 이거 설정하면 webpack-dev-server 3번대 버전에서 live reloading 동작 안함
 	// target: ['web', 'es5'],
 	devServer: {
-		liveReload: true
+		liveReload: true,
+		open: '/open.html', // 서버 시작시 열릴 페이지
 	},
 	optimization: {
 		minimizer: webpackMode === 'production' ? [
